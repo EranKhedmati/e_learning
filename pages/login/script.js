67eccb90e8e6cbd8748b9cf7c98b301c3c775183
erfankhedmati <erfankhedmati15@gmail.com>
2023-07-16 16:58:13 +0330

    add : login page , font , images , fonts

---
+++ pages/login/script.js
@@ -0,0 +1,28 @@
+// Show && hide Password ------------------------------------
+
+// Elements
+const eyeShowPath = "../../public/images/eye-show.svg",
+  eyeHidePath = "../../public/images/eye-hide.svg";
+
+const toggleEye = document.querySelectorAll(".toggle-eye");
+
+toggleEye.forEach((eye) => {
+  let isEye = false;
+  eye.addEventListener("click", () => {
+    if (isEye) {
+      eye.src = eyeHidePath;
+
+      // Prev element is = input filed
+      eye.previousElementSibling.type = "password";
+
+      isEye = !isEye;
+    } else {
+      eye.src = eyeShowPath;
+
+      // Prev element is = input filed
+      eye.previousElementSibling.type = "text";
+
+      isEye = !isEye;
+    }
+  });
+});
